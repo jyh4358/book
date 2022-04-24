13e044e1354949641cc5cf3454419a726bb5a051
jyh4358 <jyh4358@gmail.com>
2022-04-25 01:06:22 +0900

    feat: Posts 조회, 수정, 삭제 구현

--- src/main/java/com/book/service/PostsService.java
+++ src/main/java/com/book/service/PostsService.java
@@ -2,12 +2,16 @@ package com.book.service;
 
 import com.book.domain.posts.Posts;
 import com.book.domain.posts.PostsRepository;
+import com.book.web.dto.PostsListResponseDto;
 import com.book.web.dto.PostsResponseDto;
 import com.book.web.dto.PostsSaveRequestDto;
 import com.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,5 +39,18 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시물이 없습니다. id = " + id));
 
         return new PostsResponseDto(posts);
+    }
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
